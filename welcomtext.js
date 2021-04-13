9b532370d5ff636e56d10b9ac2601d9d8207e37a
Raz16 <78829707+lauan7363@users.noreply.github.com>
2021-04-12 22:54:03 -0300

    Add files via upload

---
+++ welcomtext.js
@@ -0,0 +1,31 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 
+    ━━━༺༻━━━━━༺༻━━
+💫 *JÁ CHEGA MAMANDO LAUAN*
+━━━༺༻━━━━━༺༻━━
+
+✨𝙨𝙚 𝙖𝙥𝙧𝙚𝙨𝙚𝙣𝙩𝙚 𝙘𝙤𝙢:
+
+📱 ғoтo:
+📌 ɴoмe:
+🧩 ιdαde:
+🎑 oɴde мorα:
+🪐 cor ғαvorιтα:
+💫 ιɴѕтαɢrαм:
+♒ ѕιɢɴo:
+🏳️‍🌈 ѕeхυαlιdαde:
+💍relacιonaмenтo:
+🏮 ғrαѕe qυe тe deғιɴe:
+📆 αɴιverѕαrιo:
+
+✨ʙᴇᴍ ᴠɪɴᴅᴀ(ᴏ) 
+
+
+SEJA BEM VINDO AO ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. Já vai tarde 👋`
+}
+exports.bye = bye
